a866d7eb2c6db547db519dd6afbd3fee26674a19
nitish arali <nitisharali1995@gmail.com>
2023-11-04 13:28:32 +0530

    fa

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function preventSubmit() {
 function getFormData(formObject) {
   // Replace 'https://your-google-apps-script-url' with your Google Apps Script URL
   fetch(
-    "https://script.google.com/macros/s/AKfycbzU_HDxxz3yMxR1VC9aSBDf8ngnnLGqwp4r6xF2BPeZM9gB6VC0jZqICJ4xHC1BuWCR/exec",
+    "https://script.google.com/macros/s/AKfycbzfp_x9nQoScNMeoqASi79j1cIcxv8y_Sspi2LQDd3EbLAHH9ggavZzVzvON5dniX59/exec",
     {
       method: "POST",
       body: new FormData(formObject),
